1f3f631a4197dcacefd6063bb8fca8943d797814
Marilisa Moio <moiomarilisa@gmail.com>
2024-05-23 16:24:22 +0200

    chore: create basic infrastructure

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,20 @@
 
 </head>
 <body>
+    <header class="d-flex justify-content-between p-3 mb-4 border-bottom">
+        <div class="fs-4 text-uppercase">comics db</div>
+        <ul class="nav">
+            <li class="nav-item"><a href="{{ route('home') }}" class="nav-link">Home</a></li>
+            <li class="nav-item"><a href="{{ route('comics.index')}}" class="nav-link">Vedi i Fumetti</a></li>
+            <li class="nav-item"><a href="{{ route('comics.create') }}" class="nav-link">Aggiungi un Fumetto</a></li>
+        </ul>
+    </header>
 
-    @yield('content')
-
+    <main>
+        <section>
+            <div class="container">
+                @yield('content')
+            </div>
+        </section>
+    </main>
 </body>
